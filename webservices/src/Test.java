70645ef8cc46371784f4ec752428edb25ec95dbb
flixs111@126.com <flixs111@126.com>
2017-06-09 15:00:29 +0800

    Initial commit

---
+++ webservices/src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Created by hb on 2017-06-07.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test.............");
+        System.out.println("chenggong.............");
+        System.out.println("perfect.............");
+    }
+}
