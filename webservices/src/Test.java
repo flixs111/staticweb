e4962949c51f4307e91f85485c0f20ef22447a84
flixs111@126.com <flixs111@126.com>
2017-06-09 15:13:42 +0800

    first upload11

--- webservices/src/Test.java
+++ webservices/src/Test.java
@@ -6,5 +6,8 @@ public class Test {
         System.out.println("test.............");
         System.out.println("chenggong.............");
         System.out.println("perfect.............");
+        System.out.println("perfect1.............");
+        System.out.println("perfect2.............");
+
     }
 }
